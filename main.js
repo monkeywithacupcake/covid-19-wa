02d6b67a241ea8ed8177cb7a16c9e9552e41d6b9
jess <jessachandler@gmail.com>
2020-03-08 18:14:23 -0700

    Update main.js
    add the map ... still reading in the manual data on the graphics

--- main.js
+++ main.js
@@ -15,24 +15,32 @@ fetch(ghurl)
 
 var clark = {
   name: "Clark",
+  lat: 45.7466,
+  lon: -122.5194,
   date: ["2020-03-06"],
   positive: [1],
   deaths: [0]
 };
 var grant = {
   name: "Grant",
+  lat: 47.1981,
+  lon: -119.3732,
   date: ["2020-03-05"],
   positive: [1],
   deaths: [0]
 };
 var jefferson = {
   name: "Jefferson",
+  lat: 47.7425,
+  lon: -123.304,
   date: ["2020-03-06"],
   positive: [1],
   deaths: [0]
 };
 var snohomish = {
   name: "Snohomish",
+  lat: 48.033,
+  lon: -121.8339,
   date: [
     "2020-01-21",
     "2020-02-28",
@@ -41,20 +49,25 @@ var snohomish = {
     "2020-03-03",
     "2020-03-04",
     "2020-03-05",
-    "2020-03-07"
+    "2020-03-07",
+    "2020-03-08"
   ],
-  positive: [1, 1, 1, 1, 2, 4, 9, 8],
-  deaths: [0, 0, 0, 1, 0, 0, 0, 0, 0]
+  positive: [1, 1, 1, 1, 2, 4, 9, 8, 4],
+  deaths: [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
 };
 var pierce = {
   name: "Pierce",
-  date: ["2020-03-06", "2020-03-07"],
-  positive: [1, 2],
-  deaths: [0, 0]
+  lat: 47.067,
+  lon: -122.1295,
+  date: ["2020-03-06", "2020-03-07", "2020-03-08"],
+  positive: [1, 2, 1],
+  deaths: [0, 0, 0]
 };
 
 var king = {
   name: "King",
+  lat: 47.548,
+  lon: -121.9836,
   date: [
     "2020-02-29",
     "2020-03-01",
@@ -63,10 +76,11 @@ var king = {
     "2020-03-04",
     "2020-03-05",
     "2020-03-06",
-    "2020-03-07"
+    "2020-03-07",
+    "2020-03-08"
   ],
-  positive: [3, 7, 4, 7, 10, 20, 7, 13],
-  deaths: [1, 1, 3, 3, 2, 1, 0]
+  positive: [3, 7, 4, 7, 10, 20, 7, 13, 12],
+  deaths: [1, 1, 3, 3, 2, 1, 0, 2, 2]
 };
 
 var counties = [grant, snohomish, jefferson, king, clark, pierce];
@@ -176,3 +190,67 @@ var bardead = Array.prototype.concat.apply(
 layout.title = "COVID-19 Deaths in Washington state by county (only new)";
 
 Plotly.newPlot("deathbycounty", bardead, layout);
+
+// build a map
+
+var ctyName = [];
+var ctyLon = [];
+var ctyLat = [];
+var ctyPos = [];
+var ctyDea = [];
+var hoverText = [];
+color = [, "rgb(255,65,54)", "rgb(133,20,75)", "rgb(255,133,27)", "lightgrey"];
+
+for (var i = 0; i < counties.length; i++) {
+  var pos = counties[i].positive.reduce(mySum);
+  var dea = counties[i].deaths.reduce(mySum);
+  var currentText = counties[i].name + " Pos: " + pos + " Dead: " + dea;
+  ctyName.push(counties[i].name);
+  ctyLon.push(counties[i].lon);
+  ctyLat.push(counties[i].lat);
+  ctyPos.push(pos);
+  ctyDea.push(dea);
+  hoverText.push(currentText);
+}
+var mdata = [
+  {
+    type: "scattergeo",
+    locationmode: "USA-states",
+    lat: ctyLat,
+    lon: ctyLon,
+    hoverinfo: "text",
+    text: hoverText,
+    marker: {
+      size: ctyPos,
+      line: {
+        color: "black",
+        width: 2
+      }
+    }
+  }
+];
+
+var mlayout = {
+  title: "MAP - Current WA COVID-19 (scale by # positive)",
+  showlegend: false,
+  geo: {
+    scope: "usa",
+    center: {
+      lon: -120,
+      lat: 47
+    },
+    lonaxis: { range: [-116, -125] },
+    lataxis: { range: [45, 50] },
+    showland: true,
+    landcolor: "rgb(217, 217, 217)",
+    subunitcolor: "rgb(250,255,255)",
+    countrycolor: "rgb(255,255,255)"
+  }
+};
+Plotly.newPlot(
+  "map",
+  mdata,
+  mlayout,
+  { showLink: false },
+  { displayModeBar: true }
+);
