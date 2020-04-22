cc620475de7c1a0c0ce301f3845f1b2372a21142
jess <jessachandler@gmail.com>
2020-04-21 17:55:33 -0700

    Add files via upload

--- covid.R
+++ covid.R
@@ -29,37 +29,55 @@ pos_average <- pos_data %>%
          obs = row_number()) %>%
   select(tag, obs, cum, movave)
 
-
+library(broom)
+xx <- pos_average %>% 
+  group_by(tag) %>%
+  filter(obs >= max(obs) -5) %>%
+  split(.$tag) %>% 
+  map(~lm(movave ~ obs, data = .x)) %>% 
+  map_df(tidy) %>%
+  filter(term == 'obs')
 
 # let us set up a legend for the graph
 legend <- pos_average %>%
   group_by(tag) %>%
   filter(obs == max(obs)) %>%
-  select(label = tag, y_pos = movave, x_pos = obs)
+  bind_cols(xx) %>%
+  mutate(trend = ifelse(between(estimate, -0.1,0.1), "flat", 
+                      ifelse(estimate > 0, "rising", "declining"))) %>%
+  select(label = tag, y_pos = movave, x_pos = obs, trend) 
+trend <- legend %>%
+  select(tag = label, trend)
+graph_data <- left_join(pos_average, trend)  
 
-ggplot(pos_average, aes(x = obs, y = movave, color = tag)) +
-  geom_line() + 
+ggplot(graph_data, aes(x = obs, y = movave)) +
+  geom_line(aes(color = tag)) +
   labs(title = "COVID-19 Case Curves",
        subtitle = "5 day moving average, Counties with at least 200 cases",
-       color = "County",
-       x="Days Since 3rd Case", 
-       y = "New Reported Cases per Day") + 
+       x="Days Since 3rd Case",
+       color = "Counties",
+       y = "New Reported Cases per Day",
+       caption = "Based on 35 WA Health Departments reporting as of 1800 4/21 \n data at https://github.com/monkeywithacupcake/covid-19-wa") +  
   expand_limits(x = c(0, 80)) +
   theme_minimal() + 
-  theme(legend.position = "none") +
-  geom_text(data = legend, aes(x = x_pos, y = y_pos, label = label, color = label, hjust = 0, vjust = 1)) 
+  theme(legend.justification=c(1,0), legend.position=c(1,0))  
+ggsave("~/Desktop/covid-19-wa.png")
 
-
-ggplot(pos_average, aes(x = obs, y = movave, color = tag)) +
-  geom_line() + 
+ggplot(graph_data, aes(x = obs, y = movave)) +
+  geom_line(aes(group = tag, color = trend)) + 
+  scale_color_manual(values = c("rising" = "#993333", "flat" = "#666666", "declining" = "#000055")) +
   scale_y_log10() +
   labs(title = "COVID-19 Case Curves - Log Scale",
        subtitle = "5 day moving average, Counties with at least 200 cases",
-       color = "County",
-       x="Days Since 3rd Case", 
-       y = "New Reported Cases per Day") + 
+       x="Days Since 3rd Case",
+       color = "Current Trend",
+       y = "New Reported Cases per Day",
+       caption = "Based on 35 WA Health Departments reporting as of 1800 4/21 \n data at https://github.com/monkeywithacupcake/covid-19-wa") +  
   expand_limits(x = c(0, 80)) +
   theme_minimal() + 
-  theme(legend.position = "none") +
-  geom_text(data = legend, aes(x = x_pos, y = y_pos, label = label, color = label, hjust = 0, vjust = 1)) 
- 
+  theme(legend.justification=c(1,0), legend.position=c(1,0)) +
+  geom_text(data = legend, 
+            aes(x = x_pos, y = y_pos, label = label, color = trend,
+                hjust = 0, vjust = 1),
+            show.legend = FALSE) 
+ggsave("~/Desktop/covid-19-wa-log.png") 
