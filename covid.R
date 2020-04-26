0f71f1a110e7440d84bc9a60045de2d1cd745671
jess <jessachandler@gmail.com>
2020-04-25 17:53:30 -0700

    Add files via upload
    note: covid.R code is ugly now and needs a cleanup - repeated averaging functions

--- covid.R
+++ covid.R
@@ -110,7 +110,71 @@ d_data$date <- as.Date(d_data$date)
 # remove today because missing for many health districts
 d_data <- d_data %>%
   filter(date < Sys.Date())
+d_totals <- d_data %>% 
+  group_by(tag) %>% 
+  summarise(total = sum(deaths)) 
 
+d_counties <- d_totals$tag[d_totals$total > 10]
+
+d_average <- d_data %>%
+  filter(tag %in% d_counties) %>%
+  group_by(tag) %>%
+  mutate(cumdead = cumsum(deaths)) %>%
+  filter(cumdead > 1) %>%
+  arrange(tag, date) %>%
+  mutate(d1 = lag(deaths),
+         d2 = lag(deaths, 2),
+         d3 = lag(deaths, 3),
+         d4 = lag(deaths, 4),
+         movavedead = (deaths+d1+d2+d3+d4)/5,
+         obs = row_number()) %>%
+  select(obs, cumdead, movavedead, date)
+
+library(broom)
+xx <- d_average %>% 
+  group_by(tag) %>%
+  filter(obs >= max(obs) -5) %>%
+  split(.$tag) %>% 
+  map(~lm(movavedead ~ obs, data = .x)) %>% 
+  map_df(tidy) %>%
+  filter(term == 'obs')
+
+# let us set up a legend for the graph
+legend <- d_average %>%
+  group_by(tag) %>%
+  filter(obs == max(obs)) %>%
+  bind_cols(xx) %>%
+  mutate(trend = ifelse(between(estimate, -0.1,0.1), "flat", 
+                        ifelse(estimate > 0, "rising", "declining"))) %>%
+  select(label = tag, y_d = movavedead, x_d = obs, trend) 
+trend <- legend %>%
+  select(tag = label, trend)
+graph_data <- left_join(d_average, trend)  
+
+
+ggplot(graph_data, aes(x = obs, y = movavedead)) +
+  geom_line(aes(group = tag, color = trend)) + 
+  scale_color_manual(values = c("rising" = "#993333", "flat" = "#666666", "declining" = "#000055")) +
+  scale_y_log10() +
+  labs(title = "COVID-19 Deaths Curves - Log Scale",
+       subtitle = "5 day moving average, Counties with at least 10 deaths",
+       x="Days Since 3rd Case",
+       color = "Current Trend",
+       y = "New Reported Deaths per Day",
+       caption = paste("Based on 35 WA Health Departments reporting through", 
+                       Sys.Date() - 1,
+                       "as of 1800",
+                       Sys.Date(), 
+                       "\n data at https://github.com/monkeywithacupcake/covid-19-wa",
+                       sep=" "))+  
+  expand_limits(x = c(0, 80)) +
+  theme_minimal() + 
+  theme(legend.justification=c(1,0), legend.position=c(1,0)) +
+  geom_text(data = legend, 
+            aes(x = x_d, y = y_d, label = label, color = trend,
+                hjust = 0, vjust = 1),
+            show.legend = FALSE) 
+ggsave("~/Desktop/covid-19-wa-log-deaths.png") 
 
 ## make a state aggregate
 state_pos <- pos_data %>% 
